6bb61e36540067ecfc8ea6e5c0f77ea8ccb06a90
KingGedorah <naufal.zhafari@ui.ac.id>
2024-02-25 15:27:06 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# 📚 MyJISC 📚 - Jakarta Islamic School LMS
+MyJISC merupakan LMS yang dikembangkan untuk mendukung kegiatan pembelajaran di Jakarta Islamic School 
+
+## Features
+
+## Getting Started
+
+## Contributing
+
+## Contact
---
+++ user/src/main/java/propensi/myjisc/user/controller/UserController.java
@@ -0,0 +1,21 @@
+package propensi.myjisc.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/user")
+    public Map<String, String > helloWorld() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "This is a template");
+        return response;
+    }
+}
